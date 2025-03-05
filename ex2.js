7fa3e4826fe5edef1a54c24ac4c3a88e686ead36
omiras <oscar.miras@gmail.com>
2025-03-05 20:53:03 +0100

    done ex2

--- ex2.js
+++ ex2.js
@@ -1,28 +1,34 @@
 /**
- * Que sucede aquí?
+ * ¿ Que sucede aquí? Puede que quieras coger papel y lápiz para apuntar qué valores se asigna a cada variable
  */
 
- let variable1 = 1;
- let variable2 = 2;
- let variable3 = 3;
- let variable4 = 4;
- variable2= 2;
- varialbe3= 4;
- variable4= 1;
- variable1= -7;
-
- /**
-  * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
-  */
-console.log("Valor de la variable4, apartado 1: ", variable4);
-
-
-  variable2 = variable1;
-  
-  variable4 = variable2 * 2;
-  
-  console.log("Valor de la variable4, apartado 2: ", variable4);
+let variableA = 1;
+let variableB = 2;
+let variableC = 3;
+let variableD = 4;
+variableA = 2;
+variableB = 4;
+variableC = 1;
+variableD = -7;
 
 /**
-  * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
-  */
+ * En este punto, que valor tendrá 'variableD'. ¿Por qué? ¿Cómo podrías comprobarlo? Escribe el comando que te permita comprobar el valor de la variableD
+ */
+
+// Valdrá -7 porque hemos sobreescrito el valor de la varible con ese valor en la línea 12
+console.log("Valor variableD Apartado 1: ", variableD);
+
+
+/** ---------------------------------------------- */
+
+variableD = variableD * 2;
+ 
+/**
+ * En este punto, que valor tendrá 'variableD'. ¿Por qué? ¿Cómo podrías comprobarlo? Piensa el valor que debería tener antes de mostrar su valor pantalla
+ */
+
+// Valdrá -14 porque multiplicamos el valor de la variableD por 2 y luego sobreescribimos el valor de la variableD
+console.log("Valor variableD Apartado 2: ", variableD);
+
+
+
