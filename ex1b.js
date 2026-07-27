08d765009e0f73b5f34371fb57b2ee2602427e73
omiras <oscar.miras@gmail.com>
2026-07-27 08:32:23 +0200

    done

---
+++ ex1b.js
@@ -0,0 +1,21 @@
+/**
+ * 1. Fíjate en estas declaraciones. NO LAS MODIFIQUES.
+ */
+let nombre = 'Naiara';
+let copiarNombre;
+let otroNombre;
+
+
+/**
+ * TU TRABAJO: Escribe 2 líneas código necesario para asignar el mismo valor a todas las variables (todas las variables deben contener el valor de 'Naiara')
+ */
+copiarNombre = nombre;
+otroNombre = copiarNombre;
+
+/**
+ * 2. Mostramos el valor de cada una de las variables
+ */
+
+console.log("Valor de la variable 'nombre': ", nombre);
+console.log("Valor de la variable 'copiarNombre': ", copiarNombre);
+console.log("Valor de la variable 'otroNombre': ", otroNombre);
