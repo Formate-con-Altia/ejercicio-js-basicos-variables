4a178089e455004b467f2bc14dffddf23c2e4232
omiras <oscar.miras@gmail.com>
2026-07-27 08:30:14 +0200

    Add initial implementation of variable declarations and console logs

---
+++ ex0.js
@@ -0,0 +1,12 @@
+let nombre = "Jose";
+let edad = 30;
+let edadJubilacion = edad + 37;
+let nombreCompleto = nombre + " Pérez"; 
+
+/**
+ * TU TRABAJO: A continuación, escriba 3 instrucciones JavaScript para mostrar el valor de cada una de las variables de las instrucciones de 1, 2 y 3. ¿Sabrías predecir que vas a ver por el terminal antes de ejeutar el programa?
+ */
+console.log("Valor de la variable 'nombre': ", nombre);
+console.log("Valor de la variable 'edad': ", edad);
+console.log("Valor de la variable 'edadJubilacion': ", edadJubilacion);
+console.log("Valor de la variable 'nombreCompleto': ", nombreCompleto);
